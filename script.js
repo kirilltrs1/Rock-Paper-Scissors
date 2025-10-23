17beb9639ec09c251bc2056c2badcaf84d180e33
kirilltrs1 <kirill.tarasyuk@outlook.com>
2025-10-23 14:17:36 +0300

    my brain is fucked that's pseudo end solution

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
-
-
 function getComputerChoice () {
-    const whatIsIt = ["rock", "paper", "scissors"];
-    return Math.floor(Math.random(whatIsIt.length));
+    const arrayStrings = ["rock", "paper", "scissors"];
+    const randomizer = Math.floor(Math.random()*3);
+    if (randomizer === 0) {
+        return arrayStrings[0]
+    }
+    else if (randomizer === 1) {
+        return arrayStrings[1]
+    }
+    else {
+        return arrayStrings[2]
+    }
 }
 
-const message = getComputerChoice()
-console.log(message)
-console.log([whatIsIt.length]);
+function getHumanChoice () {
+    const humanChoice = prompt("Rock, scissors or paper?").toLowerCase().trim();
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let result;
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "scissors") {
+                result = "You win! Rock beats Scissors";
+                humanScore++;
+            } else if (computerChoice === "paper") {
+                result = "You lose! Paper beats Rock";
+                computerScore++;
+            } else {
+                result = "It's a tie!";
+            }
+            break;
+        
+        case "scissors":
+            if (computerChoice === "paper") {
+                result = "You win! Scissors beats Paper";
+                humanScore++;
+            } else if (computerChoice === "rock") {
+                result = "You lose! Rock beats Scissors";
+                computerScore++;
+            } else {
+                result = "It's a tie!";
+            }
+            break;
+        
+        case "paper":
+            if (computerChoice === "rock") {
+                result = "You win! Paper beats Rock";
+                humanScore++;
+            } else if (computerChoice === "scissors") {
+                result = "You lose! Scissors beats Paper";
+                computerScore++;
+            } else {
+                result = "It's a tie!";
+            }
+            break;
+        
+        default:
+            result = "Invalid choice!";
+    }
+    
+    return result;
+}
+
+
+
+
+function playGame() {
+    for (let i=1; i<=5; i++) {              
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+}
